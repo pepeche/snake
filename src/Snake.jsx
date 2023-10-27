5da12d840646380687deb23b26b5008abb92d728
Vincent Péchon <vincent.pechon37@gmail.com>
2023-10-27 10:11:31 +0200

    fix move snake

--- src/Snake.jsx
+++ src/Snake.jsx
@@ -1,4 +1,3 @@
-// import React, { useState, useEffect, useCallback } from "react";
 import { useEffect, useRef, useState } from "react";
 import "./Snake.css";
 
@@ -6,76 +5,76 @@ function Snake({width, height}) {
   
   const canvasRef = useRef(null);
   
-  let [xPos, setxPos] = useState(0);
-  let [yPos, setyPos] = useState(0);
+  let [xPos, setxPos] = useState(60);
+  let [yPos, setyPos] = useState(60);
   let [direction, setDirection] = useState("right");
-  
   
   useEffect(()=>{
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
-    if(xPos + 20 > width || xPos < 0){
+    if(xPos + 20 > width || xPos < 0 || yPos + 20 > height || yPos < 0){
       alert(xPos)
-      setxPos(0);
-      setyPos(0)
+      setDirection("right")
+      setxPos(60);
+      setyPos(60);
       ctx.fillRect(xPos,yPos,20,20);
       ctx.fillStyle = "green";
     } else {
       ctx.fillRect(xPos,yPos,20,20);
       ctx.fillStyle = "green";
-      // console.log(xPos, yPos)
     }
-    setTimeout(() => {
-      if(direction = "right"){
+
+    const interval = setInterval(() => {
+      if(direction === "right"){
+        console.log(direction, xPos, yPos)
         setxPos((prevxPos) => prevxPos + 20)
         ctx.clearRect(xPos, yPos, width, height)
       }
-      else if (direction = "bottom"){
+      else if (direction === "bottom"){
+        console.log(direction, xPos, yPos)
         setyPos((prevyPos) => prevyPos + 20);
         ctx.clearRect(xPos, yPos, width, height)
       }
-      else if (direction = "left") {
+      else if (direction === "left") {
+        console.log(direction, xPos, yPos)
         setxPos((prevxPos) => prevxPos - 20)
         ctx.clearRect(xPos, yPos, width, height)
       } 
       else{
+        console.log(direction, xPos, yPos)
         setyPos((prevyPos) => prevyPos - 20);
         ctx.clearRect(xPos, yPos, width, height)
       }
-    }, 1000);
+    }, 500);
+    window.addEventListener('keydown', moveSnake);
+    return () => {
+      window.removeEventListener('keydown', moveSnake);
+      clearInterval(interval)
+    };
   }, [xPos, yPos, direction])
 
   function moveSnake(event) {
+    console.log(direction)
     if (event.key === 'ArrowRight') {
-      direction = "right" ?  setDirection("bottom") : 
-      direction = "bottom" ?  setDirection("left") : 
-      direction = "left" ?  setDirection("top") : setDirection("right");
-      console.log(direction)
+      direction === "right" ?  setDirection("bottom") : 
+      direction === "bottom" ?  setDirection("left") : 
+      direction === "left" ?  setDirection("top") : setDirection("right");
+      console.log("arrowright", direction)
     } 
     else if (event.key === 'ArrowLeft') {
-      direction = "right" ?  setDirection("bottom") : 
-      direction = "bottom" ?  setDirection("left") : 
-      direction = "left" ?  setDirection("top") : setDirection("right");
-      console.log(direction)
+      direction === "right" ?  setDirection("top") : 
+      direction === "top" ?  setDirection("left") : 
+      direction === "left" ?  setDirection("bottom") : setDirection("right");
+      console.log("arrowleft", direction)
     }
   }
-
-  useEffect(() => {
-    // Ajoutez le gestionnaire d'événements à la fenêtre
-    window.addEventListener('keydown', moveSnake);
-
-    // Nettoyez le gestionnaire d'événements lorsque le composant est démonté
-    return () => {
-      window.removeEventListener('keydown', moveSnake);
-    };
-  }, []);
 
   return (
     <canvas
       className="snakeSprite"
       ref={canvasRef}
-      width={width} // Largeur du canvas
-      height={height} // Hauteur du canvas
+      width={width}
+      height={height}
     >
     </canvas>
       );
