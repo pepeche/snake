2714a41b2110ebb4b4ec913b12a1f5ca5978275b
Vincent Péchon <vincent.pechon37@gmail.com>
2023-10-27 09:26:07 +0200

    test snake direction

--- src/Gameboard.jsx
+++ src/Gameboard.jsx
@@ -1,29 +1,24 @@
-// import "./Gameboard.css";
-
-// function Gameboard() {
-//   return (
-//     <div className="gameboardGlobal">
-//       <Snake />
-//     </div>
-//   );
-// }
-
-// export default Gameboard;
-
 import React, { useRef, useEffect, useState } from "react";
 import Snake from "./Snake.jsx";
 import "./Gameboard.css"
 
 function CanvasWithCoordinates({ gridSizeGlobal }) {
+
+  
   const canvasRef = useRef(null);
   const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
-  const handleMouseMove = (e) => {
+  
+  
+  function handleKeyPress(e) {
+    console.log("coucou")
     const canvas = canvasRef.current;
     const rect = canvas.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
     setCoordinates({ x, y });
   };
+
+
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
@@ -46,7 +41,7 @@ function CanvasWithCoordinates({ gridSizeGlobal }) {
       ctx.beginPath();
       ctx.moveTo(0, y);
       ctx.lineTo(canvasWidth, y);
-      ctx.strokeStyle = "#FF0000"; // Couleur des axes
+      ctx.strokeStyle = "black"; // Couleur des axes
       ctx.stroke();
     }
     // Dessinez des lignes verticales
@@ -54,64 +49,33 @@ function CanvasWithCoordinates({ gridSizeGlobal }) {
       ctx.beginPath();
       ctx.moveTo(x, 0);
       ctx.lineTo(x, canvasHeight);
-      ctx.strokeStyle = "#FF0000";
+      ctx.strokeStyle = "black";
       ctx.stroke();
     }
   }, [coordinates, gridSizeGlobal]);
 
+  let width = 400;
+  let height = 400;
 
-
-  let [left, setLeft] = useState(0);
-  let [top, setTop] = useState(0);
-  let [right, setRight] = useState(0);
-  let [bottom, setBottom] = useState(0);
-
-  // useEffect(() => {
-  //   if (event.key === 'ArrowLeft') {
-  //     setLeft = left+20;
-  //     console.log('Touche "Left" pressée');
-  //     // Ajoutez ici le code que vous souhaitez exécuter pour la touche "Left"
-  // }}, [left]);
-
-  function handleKeyPress(event) {
-    if (event.key === 'ArrowLeft') {
-      setLeft((prevLeft) => prevLeft + 20)
-      console.log('Touche "Left" pressée');
-      // Ajoutez ici le code que vous souhaitez exécuter pour la touche "Left"
-    } else if (event.key === 'ArrowRight') {
-      // La touche "Right" a été pressée
-      console.log('Touche "Right" pressée');
-      // Ajoutez ici le code que vous souhaitez exécuter pour la touche "Right"
-    }
-  }
   
-  useEffect(() => {
-    // Ajoutez le gestionnaire d'événements à la fenêtre
-    window.addEventListener('keydown', handleKeyPress);
-
-    // Nettoyez le gestionnaire d'événements lorsque le composant est démonté
-    return () => {
-      window.removeEventListener('keydown', handleKeyPress);
-    };
-  }, []);
 
   return (
     <div className="container">
       <p>Survolez le canvas pour afficher les coordonnées des axes X et Y.</p>
       <p>Coordonnées X : {coordinates.x}</p>
       <p>Coordonnées Y : {coordinates.y}</p>
-      <div className="buttons">
-        <button>Right</button>
-      </div>
       <div id="gameBoardContainer">
         <canvas
           id="gameBoard"
           ref={canvasRef}
-          width={400} // Largeur du canvas
-          height={400} // Hauteur du canvas
-          onMouseMove={handleMouseMove}
+          width={width} // Largeur du canvas
+          height={height} // Hauteur du canvas
+          
+          style={{
+            outline : "5px solid black"
+          }}
         ></canvas>
-        <Snake left={left} top={top} />
+        <Snake width={width} height={height}/>
       </div>
       
     </div>
--- src/Snake.jsx
+++ src/Snake.jsx
@@ -1,14 +1,84 @@
 // import React, { useState, useEffect, useCallback } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./Snake.css";
 
-function Snake({left, top}) {
-  return <div 
-    className="snakeSprite"
-    style = {{
-      left : `${left}px`,
-      top : `${top}px`
-    }}
-  ></div>;
+function Snake({width, height}) {
+  
+  const canvasRef = useRef(null);
+  
+  let [xPos, setxPos] = useState(0);
+  let [yPos, setyPos] = useState(0);
+  let [direction, setDirection] = useState("right");
+  
+  
+  useEffect(()=>{
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    if(xPos + 20 > width || xPos < 0){
+      alert(xPos)
+      setxPos(0);
+      setyPos(0)
+      ctx.fillRect(xPos,yPos,20,20);
+      ctx.fillStyle = "green";
+    } else {
+      ctx.fillRect(xPos,yPos,20,20);
+      ctx.fillStyle = "green";
+      // console.log(xPos, yPos)
+    }
+    setTimeout(() => {
+      if(direction = "right"){
+        setxPos((prevxPos) => prevxPos + 20)
+        ctx.clearRect(xPos, yPos, width, height)
+      }
+      else if (direction = "bottom"){
+        setyPos((prevyPos) => prevyPos + 20);
+        ctx.clearRect(xPos, yPos, width, height)
+      }
+      else if (direction = "left") {
+        setxPos((prevxPos) => prevxPos - 20)
+        ctx.clearRect(xPos, yPos, width, height)
+      } 
+      else{
+        setyPos((prevyPos) => prevyPos - 20);
+        ctx.clearRect(xPos, yPos, width, height)
+      }
+    }, 1000);
+  }, [xPos, yPos, direction])
+
+  function moveSnake(event) {
+    if (event.key === 'ArrowRight') {
+      direction = "right" ?  setDirection("bottom") : 
+      direction = "bottom" ?  setDirection("left") : 
+      direction = "left" ?  setDirection("top") : setDirection("right");
+      console.log(direction)
+    } 
+    else if (event.key === 'ArrowLeft') {
+      direction = "right" ?  setDirection("bottom") : 
+      direction = "bottom" ?  setDirection("left") : 
+      direction = "left" ?  setDirection("top") : setDirection("right");
+      console.log(direction)
+    }
+  }
+
+  useEffect(() => {
+    // Ajoutez le gestionnaire d'événements à la fenêtre
+    window.addEventListener('keydown', moveSnake);
+
+    // Nettoyez le gestionnaire d'événements lorsque le composant est démonté
+    return () => {
+      window.removeEventListener('keydown', moveSnake);
+    };
+  }, []);
+
+  return (
+    <canvas
+      className="snakeSprite"
+      ref={canvasRef}
+      width={width} // Largeur du canvas
+      height={height} // Hauteur du canvas
+    >
+    </canvas>
+      );
 }
 
 export default Snake;
